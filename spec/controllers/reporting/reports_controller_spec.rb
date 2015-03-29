e0e43a9166d469b7da1bd0df76c21583c20da774
Xudong Liu <xudong.camsys@gmail.com>
2015-03-28 20:13:52 -0400

    specify routes in controller test

--- spec/controllers/reporting/reports_controller_spec.rb
+++ spec/controllers/reporting/reports_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 module Reporting
   RSpec.describe ReportsController, type: :controller do
+    routes { Reporting::Engine.routes }
 
     describe "GET #index" do
       it "returns http success" do

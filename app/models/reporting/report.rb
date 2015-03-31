ecc50cfbb3907c3b41ee7563cacfb563f94368bf
Xudong Liu <xudong.camsys@gmail.com>
2015-03-31 16:01:17 -0400

    implemented creating a AR model class for each reporting table so can leverage ransack utilities

--- app/models/reporting/report.rb
+++ app/models/reporting/report.rb
@@ -1,5 +1,49 @@
 module Reporting
   class Report < ActiveRecord::Base
     has_many :fields
+
+    validates :name, presence: true
+    validates :data_source, presence: true
+
+    # model name is based on table name
+    def data_model_class_name
+      "Reporting::#{data_source.classify}"
+    end
+
+    def data_model
+      define_data_model data_source, fields
+    end
+
+    private
+
+    # define new model for the tables not known to AR
+    def define_data_model(table_name, fields)
+
+      if !Object.const_defined?(data_model_class_name)
+        klass = Class.new ActiveRecord::Base do 
+
+          # configure table/view name
+          self.table_name = table_name
+
+          # model should be readonly (since this is a reporting tool)
+          def readonly?
+            true
+          end
+
+        end
+
+        # configure new model name
+        Reporting.const_set table_name.classify, klass
+      end
+
+      # define customized ransackers
+      fields.each do |field|
+        field.ransacker
+      end
+
+      # return defined model
+      Object.const_get data_model_class_name
+    end
+
   end
 end

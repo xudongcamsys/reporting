331cf9f71749b72623951e5cd712283338e00a8f
Xudong Liu <xudong.camsys@gmail.com>
2015-03-31 16:06:43 -0400

    remove results action from reports_controller; was used for testing

--- app/controllers/reporting/reports_controller.rb
+++ app/controllers/reporting/reports_controller.rb
@@ -14,12 +14,6 @@ module Reporting
       # find out filter_groups
       @filter_groups = FilterGroup.where(id: @report.fields.pluck(:filter_group_id).uniq)
     end
-
-    def results
-      @report = Report.find(params[:report_id])
-      @q = @report.data_model.ransack(params[:q])
-
-      @results = @q.result
-    end
+    
   end
 end

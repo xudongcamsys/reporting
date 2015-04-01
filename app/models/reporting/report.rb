0728e03f1cba7c2c537acc462055526070312ad7
Xudong Liu <xudong.camsys@gmail.com>
2015-04-01 13:44:19 -0400

    move to_csv out of modelable module

--- app/models/reporting/report.rb
+++ app/models/reporting/report.rb
@@ -23,7 +23,11 @@ module Reporting
     def define_data_model
 
       # call modelable module method
-      make_a_reporting_model(data_model_class_name, data_source)
+      make_a_reporting_model data_model_class_name, data_source
+
+      # make sure data_model can export as csv
+      reporting_model = Object.const_get data_model_class_name
+      enable_csv_export_on_data_model reporting_model
 
       # define customized ransackers
       filter_fields.each do |field|
@@ -31,7 +35,48 @@ module Reporting
       end
 
       # return defined model
-      Object.const_get data_model_class_name
+      reporting_model
+
+    end
+
+    # enable to_csv on each newly created reporting_data_model
+    def enable_csv_export_on_data_model(reporting_model)
+
+      if reporting_model
+
+        reporting_model.class_exec {
+
+          def self.to_csv(options = {})
+
+            # figure out output field names and columns
+            output_field_names = []
+            output_field_titles = []
+            # if output_fields is empty, then use column_names instead
+            report = Reporting::Report.where(data_source: self.table_name).first
+            output_fields = report.output_fields rescue []
+
+            if output_fields.blank?
+              output_field_names = self.column_names
+              output_field_titles = output_field_names
+            else
+              output_fields.each do |field|
+                output_field_names << field.name
+                output_field_titles << field.title.blank? ? field.name : field.title
+              end
+            end
+
+            CSV.generate(options) do |csv|
+              csv << output_field_titles
+              all.each do |row|
+                csv << row.attributes.values_at(*output_field_names)
+              end
+            end
+
+          end
+
+        }
+      end
+
     end
 
   end
--- lib/reporting/modelable.rb
+++ lib/reporting/modelable.rb
@@ -16,15 +16,6 @@ module Reporting
             true
           end
 
-          def self.to_csv(options = {})
-            CSV.generate(options) do |csv|
-              csv << column_names
-              all.each do |row|
-                csv << row.attributes.values_at(*column_names)
-              end
-            end
-          end
-
         end
 
         # configure new model name

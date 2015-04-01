6c76e9ce0968e7e37d878a402bf97ed56a084fc5
Xudong Liu <xudong.camsys@gmail.com>
2015-04-01 13:45:02 -0400

    show all columns when output_fields is empty

--- app/controllers/reporting/results_controller.rb
+++ app/controllers/reporting/results_controller.rb
@@ -7,19 +7,27 @@ module Reporting
       @q = @report.data_model.ransack(params[:q])
       @params = {q: params[:q]}
 
-      # TODO: if output_fields is empty, then use .attributes
-      @fields = @report.output_fields
-
       begin
         @results = @q.result
 
         # this is used to test if any sql exception is triggered in querying
         # commen errors: table not found
         first_result = @results.first 
+
+        # list all output fields
+        # if output_fields is empty, then export all columns in this table
+        @fields = @report.output_fields.blank? ?
+          @report.data_model.column_names.map{
+            |x| {
+              name: x 
+            }
+          } : @report.output_fields
+
       rescue => e
         # error message handling
 
         @results = []
+        @fields = []
       end
 
       respond_to do |format|
